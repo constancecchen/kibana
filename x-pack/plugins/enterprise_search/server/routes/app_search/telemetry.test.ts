e414be5d47a5dcfb864924ce159f41e88c319b3e
Constance <constancecchen@users.noreply.github.com>
2020-06-04 12:01:29 -0700

    Fix Typescript errors (#16)
    * Fix public mocks
    
    * Fix empty states types
    
    * Fix engine table component errors
    
    * Fix engine overview component errors
    
    * Fix setup guide component errors
    
    - SetBreadcrumbs will be fixed in a separate commit
    
    * Fix App Search index errors
    
    * Fix engine overview header component errors
    
    * Fix applications context index errors
    
    * Fix kibana breadcrumb helper errors
    
    * Fix license helper errors
    
    * :exclamation: Refactor React Router EUI link/button helpers
    - in order to fix typescript errors
    
    - this changes the component logic significantly to a react render prop, so that the Link and Button components can have different types - however, end behavior should still remain the same
    
    * Fix telemetry helper errors
    
    * Minor unused var cleanup in plugin files
    
    * Fix telemetry collector/savedobjects errors
    
    * Fix MockRouter type errors and add IRouteDependencies export
    
    - routes will use IRouteDependencies in the next few commits
    
    * Fix engines route errors
    
    * Fix telemetry route errors
    
    * Remove any type from source code
    
    - thanks to Scotty for the inspiration
    
    * Add eslint rules for Enterprise Search plugin
    
    - Add checks for type any, but only on non-test files
    - Disable react-hooks/exhaustive-deps, since we're already disabling it in a few files and other plugins also have it turned off
    
    * Cover uncovered lines in engines_table and telemetry tests

--- x-pack/plugins/enterprise_search/server/routes/app_search/telemetry.test.ts
+++ x-pack/plugins/enterprise_search/server/routes/app_search/telemetry.test.ts
@@ -26,13 +26,13 @@ describe('App Search Telemetry API', () => {
       router: mockRouter.router,
       getSavedObjectsService: () => savedObjectsServiceMock.create(),
       log: mockLogger,
-    });
+    } as any);
   });
 
   describe('PUT /api/app_search/telemetry', () => {
     it('increments the saved objects counter', async () => {
       const successResponse = { success: true };
-      incrementUICounter.mockImplementation(jest.fn(() => successResponse));
+      (incrementUICounter as jest.Mock).mockImplementation(jest.fn(() => successResponse));
 
       await mockRouter.callRoute({ body: { action: 'viewed', metric: 'setup_guide' } });
 
@@ -45,11 +45,25 @@ describe('App Search Telemetry API', () => {
     });
 
     it('throws an error when incrementing fails', async () => {
-      incrementUICounter.mockImplementation(jest.fn(() => Promise.reject('Failed')));
+      (incrementUICounter as jest.Mock).mockImplementation(jest.fn(() => Promise.reject('Failed')));
 
       await mockRouter.callRoute({ body: { action: 'error', metric: 'error' } });
 
       expect(incrementUICounter).toHaveBeenCalled();
+      expect(mockLogger.error).toHaveBeenCalled();
+      expect(mockRouter.response.internalError).toHaveBeenCalled();
+    });
+
+    it('throws an error if the Saved Objects service is unavailable', async () => {
+      jest.clearAllMocks();
+      registerTelemetryRoute({
+        router: mockRouter.router,
+        getSavedObjectsService: null,
+        log: mockLogger,
+      } as any);
+      await mockRouter.callRoute({});
+
+      expect(incrementUICounter).not.toHaveBeenCalled();
       expect(mockLogger.error).toHaveBeenCalled();
       expect(mockRouter.response.internalError).toHaveBeenCalled();
     });
